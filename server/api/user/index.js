07ed8266126b802b5eb7912ac7ad3ea01e780025
Masaud Alhassan <sophismay@gmail.com>
2016-06-12 18:46:58 +0200

    Update index.js

--- server/api/user/index.js
+++ server/api/user/index.js
@@ -6,8 +6,6 @@ import auth from '../../auth/auth.service';
 
 var router = express.Router();
 
-router.get('/', controller.index);
-
 router.get('/me', auth.isAuthenticated(), controller.me);
 router.get('/:id', auth.isAuthenticated(), controller.show);
 router.post('/', controller.create);
